c5a00d5de7eed91f31e5102a59e1065adecba193
Elizaveta Kosiachenko <ekosyachenko@gmail.com>
2017-06-04 16:34:34 -0700

    added Andrews changes to mallet call in the bash script

--- arch_recovery.sh
+++ arch_recovery.sh
@@ -2,7 +2,7 @@
 java -jar Crawler/target/Crawler-1.0-SNAPSHOT-jar-with-dependencies.jar
 #run the mallet
 ./mallet-2.0.8/bin/mallet import-dir --input ./Crawler/output --keep-sequence --remove-stopwords --output arch.mallet
-./mallet-2.0.8/bin/mallet train-topics --input arch.mallet --num-topics 1 --optimize-interval 1 --output-state topic-state.gz  --output-topic-keys arch_keys.txt --output-doc-topics arch_composition.txt
+./mallet-2.0.8/bin/mallet train-topics --input arch.mallet --num-topics 5 --num-top-words 2 --optimize-interval 10 --output-state topic-state.gz  --output-topic-keys arch_keys.txt --output-doc-topics arch_composition.txt
 
 rm topic-state.gz
 rm arch.mallet
